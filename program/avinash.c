7941e83ceeca8e215609e0646d342df4626ac694
Avinash20031999 <101059765+Avinash20031999@users.noreply.github.com>
2022-03-15 21:45:08 +0530

    Update avinash.c

--- program/avinash.c
+++ program/avinash.c
@@ -2,8 +2,10 @@
 int main()
 {
     int a, b, sum;
-    printf("enter the numbers\n");
-    scanf("%d %d", &a,&b);
+    printf("enter the first number\n");
+    scanf("%d", &a);
+    printf("enter the second number\n");
+    scanf("%d", &b);
     sum = a+b;
     printf("%d\n", sum);
     return 0;
