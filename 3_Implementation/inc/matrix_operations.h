2958ca1f65427e6f0689ec6956f31885e7661ffd
Avinash20031999 <101059765+Avinash20031999@users.noreply.github.com>
2022-04-02 23:58:56 +0530

    Update matrix_operations.h

--- 3_Implementation/inc/matrix_operations.h
+++ 3_Implementation/inc/matrix_operations.h
@@ -1,11 +1,8 @@
 /**
- * @file matrix_operations.h
- * @brief A program to calculate various operations on a matrix
- * @version 0.1
- * @date 2021-04-13
- * 
- * @copyright Copyright (c) 2021
- * 
+ * File matrix_operations.h
+ * A program to calculate various operations on a matrix
+ * Version 1.0
+ * Date 25 Mar 2022
  */
 #ifndef __MATRIX_OPERATIONS_H__
 #define __MATRIX_OPERATIONS_H__
@@ -15,14 +12,13 @@
 
 
 /**
- * @brief Enum for success or failure of test case
- * 
+ Enum for success or failure of test case
+  
  */
 typedef enum{SUCCESS=1,FAILURE=0}error_t;
 
 /**
- * @brief A structure containing matrix elements
- * 
+ A structure containing matrix elements 
  */
 typedef struct{
 
@@ -33,96 +29,62 @@ float result;
 }mat;
 
 /**
- * @brief A function to allocate memory dynamically for the matrix to be inputted
- * 
- * @param matrix 
- * @param n 
- * @return float** 
- */
+ A function to allocate memory dynamically for the matrix to be inputted
+ 
 float ** alloc_input_matrix(float **matrix, int n);
 
 /**
- * @brief Dynamically allocate memory for matrix
- * 
- * @param matrix 
- * @param n 
+ Dynamically allocate memory for matrix
+ 
  */
 void dynamic_alloc_mat(float **matrix,int n);
 
 /**
- * @brief To print the matrix on the screen
- * 
- * @param matrix 
- * @param n 
+ To print the matrix on the screen
+ 
  */
 void output_matrix(float **matrix,int n);
 
 /**
- * @brief Free dynamically allocated memory space
- * 
- * @param m 
+ Free dynamically allocated memory space
+ 
  */
 void free_matrix_structure(mat *m);
 
-/**
- * @brief 
- * 
- * @param matrix1 
- * @param matrix2 
- * @param n 
- * @return error_t 
- */
+
 error_t add_matrices(float **matrix1,float **matrix2,int n);
 
 /**
- * @brief To find the difference of the matrices
- * 
- * @param matrix1 
- * @param matrix2 
- * @param n 
- * @return error_t 
+ To find the difference of the matrices
+ 
  */
 error_t subtract_matrices(float **matrix1,float **matrix2, int n);
 /**
- * @brief To find the product of the matrices
- * 
- * @param matrix1 
- * @param matrix2 
- * @param n 
- * @return error_t
+ To find the product of the matrices
+  
  */
 error_t product_matrices(float **matrix1,float **matrix2, int n);
 /**
- * @brief To find the determinant of the matrix
- * 
- * @param matrix 
- * @return float 
+ To find the determinant of the matrix
+  
  */
 float determinant(float **matrix,int k);
 
 /**
- * @brief To find the transpose of a matrix
- * 
- * @param matrix 
- * @param n 
- * @return error_t
+ To find the transpose of a matrix
+  
  */
 error_t transpose(float **matrix,int n);
 
 /**
- * @brief To find the inverse of a matrix
- * 
- * @param num 
- * @param f 
- * @return error_t 
+To find the inverse of a matrix
+  
  */
 error_t inverse(float **num, int f);
 
 /**
- * @brief Power of minus one
- * 
- * @param exp 
- * @return float 
+ Power of minus one
+  
  */
 float power(int exp);
 
